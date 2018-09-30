b0140e2be66d5b1f61b13342859f5b4016e644bf
Jeany <469420741@qq.com>
2018-09-30 13:24:04 +0800

    ok

--- js/common.js
+++ js/common.js
@@ -3,3 +3,18 @@ function Rem(){
 }
 Rem();
 window.addEventListener('resize',Rem);
+
+window.onload = function () {
+	var menuBtn = document.querySelector('.option-h5 .menu');
+	var h5Slide = document.querySelector('.h5-slide');
+	var slideBox = document.querySelector('.h5-slide .slide-box');
+	menuBtn.onclick = function () {
+		h5Slide.classList.add('active');
+	}
+	h5Slide.onclick = function () {
+		h5Slide.classList.remove('active');
+	}
+	slideBox.onclick = function(e) {
+		e.stopPropagation();
+	}
+}
